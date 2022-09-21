9024d5c1d593e0f9abc2aff81e3be72230fef4d9
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2022-09-20 19:53:52 -0700

    Better insertion and removal

--- scripts/CassetteHolder.gd
+++ scripts/CassetteHolder.gd
@@ -2,5 +2,17 @@ extends Spatial
 
 const Cassette = preload("Cassette.gd")
 
+var holding = false
+
 func can_take(object: Spatial) -> bool:
 	return object is Cassette
+
+func on_insert(obj):
+	holding = true
+
+func on_remove(obj):
+	holding = false
+
+func _process(delta):
+	if holding:
+		$Mesh.rotation.y += delta
--- scripts/input/HandState.gd
+++ scripts/input/HandState.gd
@@ -29,16 +29,17 @@ func is_holding_anything():
 func process(delta: float):
 	animations.process(delta)
 
-func physics_process(object: RaycastResult, container: RaycastResult):
+func physics_process(object_ray: RaycastResult, container_ray: RaycastResult):
 	if pick_up_requested:
 		pick_up_requested = false
-		var obj = object_hit_by_raycast_result(object)
+		var obj = object_hit_by_raycast_result(object_ray)
 		pick_up_object(obj)
-	var target_transform = object_place_transform(object, container)
+	var container = get_targeted_container(container_ray)
+	var target_transform = object_place_transform(object_ray, container)
 	update_cursor(target_transform)
 	if put_down_requested:
 		put_down_requested = false
-		put_down_object(target_transform)
+		put_down_object(target_transform, container)
 
 func object_hit_by_raycast_result(result):
 	if result.collider != null:
@@ -51,17 +52,35 @@ func object_with_collider(collider):
 
 func pick_up_object(obj):
 	if obj != null and obj.has_method("on_pick_up"):
+		if not try_remove_object_from_container(obj):
+			return
+		obj.contained_in = null
 		obj.on_pick_up()
 		holding = obj
 		animations.start(PickUpAnim.new(held_parent), obj)
 
-func object_place_transform(object: RaycastResult, container: RaycastResult):
-	if container.collider != null:
-		return object_with_collider(container.collider)\
-			.get_node("InsertionPoint") \
-			.global_transform
-	elif object.collider != null:
-		return Transform.IDENTITY.translated(object.position)
+func try_remove_object_from_container(obj):
+	if obj.contained_in != null:
+		if obj.contained_in.has_method("can_remove"):
+			if not obj.contained_in.can_remove(obj):
+				return false
+		if obj.contained_in.has_method("on_remove"):
+			obj.contained_in.on_remove(obj)
+	return true
+
+func get_targeted_container(container_ray: RaycastResult):
+	if container_ray.collider != null:
+		var container = object_with_collider(container_ray.collider)
+		if container.has_method("can_take"):
+			if container.can_take(holding):
+				return container
+	return null
+
+func object_place_transform(object_ray: RaycastResult, container):
+	if container != null:
+		return container.get_node("InsertionPoint").global_transform
+	elif object_ray.collider != null:
+		return Transform.IDENTITY.translated(object_ray.position)
 	else:
 		return null
 
@@ -74,11 +93,15 @@ func update_cursor(target_transform):
 		place_cursor.mesh = mesh_obj.mesh
 		place_cursor.transform = target_transform * mesh_obj.transform
 
-func put_down_object(target_transform):
+func put_down_object(target_transform, container):
 	assert(holding != null)
 	if target_transform == null:
 		return
 	if holding.has_method("on_put_down"):
 		holding.on_put_down()
+	if container != null:
+		holding.contained_in = container
+		if container.has_method("on_insert"):
+			container.on_insert(holding)
 	animations.start(PutDownAnim.new(target_transform), holding)
 	holding = null
