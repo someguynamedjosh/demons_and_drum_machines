e8242f16ea82edf2fc4a9f8a9fb145b72bdee220
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2022-09-20 18:57:54 -0700

    Smooth ghost handling

--- scripts/input/GameCamera.gd
+++ scripts/input/GameCamera.gd
@@ -6,15 +6,19 @@ const HoverState = preload("HoverState.gd")
 const InputHandler = preload("InputHandler.gd")
 const InteractState = preload("InteractState.gd")
 
+export var place_cursor: NodePath = "PlaceCursor"
+
 var crosshair: Crosshair = Crosshair.new(self)
 var interact_state: InteractState = InteractState.new()
-onready var hand_state: HandState = HandState.new($HeldParent)
+onready var hand_state: HandState \
+	= HandState.new($HeldParent, get_node(place_cursor))
 onready var hover_state: HoverState = HoverState.new(hand_state)
-onready var input_handler: InputHandler = InputHandler.new(self, hand_state, interact_state)
+onready var input_handler: InputHandler \
+	= InputHandler.new(self, hand_state, interact_state)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	input_handler.ready()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -24,6 +28,7 @@ func _input(event: InputEvent):
 	input_handler.input(event)
 
 func _physics_process(_delta):
+	input_handler.physics_update()
 	var raycast_result = crosshair.raycast()
 	hover_state.physics_process(raycast_result)
 	interact_state.physics_process(raycast_result)
--- scripts/input/HandState.gd
+++ scripts/input/HandState.gd
@@ -5,14 +5,16 @@ const PickUpAnim = preload('res://scripts/animation/PickUpAnim.gd')
 const PutDownAnim = preload('res://scripts/animation/PutDownAnim.gd')
 
 var held_parent: Spatial
+var place_cursor: MeshInstance
 var holding: Spatial
 var pick_up_requested = false
 var put_down_requested = false
 var animations = HandAnimationSystem.new()
 
-func _init(h: Spatial):
+func _init(h: Spatial, c: MeshInstance):
 	assert(h != null)
 	held_parent = h
+	place_cursor = c
 
 func request_pick_up():
 	pick_up_requested = true
@@ -31,6 +33,7 @@ func physics_process(crosshair_raycast_result: Dictionary):
 		pick_up_requested = false
 		var obj = object_hit_by_raycast_result(crosshair_raycast_result)
 		pick_up_object(obj)
+	update_cursor(crosshair_raycast_result)
 	if put_down_requested:
 		put_down_requested = false
 		put_down_object(crosshair_raycast_result)
@@ -49,6 +52,17 @@ func pick_up_object(obj):
 		obj.on_pick_up()
 		holding = obj
 		animations.start(PickUpAnim.new(held_parent), obj)
+
+func update_cursor(crosshair_raycast_result: Dictionary):
+	if holding == null or not('position' in crosshair_raycast_result):
+		place_cursor.hide()
+	else:
+		place_cursor.show()
+		var mesh_obj = holding.get_node("Mesh")
+		place_cursor.mesh = mesh_obj.mesh
+		place_cursor.transform \
+			= Transform.IDENTITY.translated(crosshair_raycast_result.position) \
+				* mesh_obj.transform
 
 func put_down_object(crosshair_raycast_result):
 	assert(holding != null)
